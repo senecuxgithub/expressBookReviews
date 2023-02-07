b33c131d7865636611c3095b817c494258f25d0d
senecuxgithub <120370015+senecuxgithub@users.noreply.github.com>
2023-02-07 08:36:42 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
 let books = require("./booksdb.js");
- 
 
 const regd_users = express.Router();                        
 
@@ -29,12 +28,11 @@ const authenticatedUser =  (username,password)=>{
     return false;
   }
 }
-// Task 7:     Complete the code for logging in as a registered user.				Task 7:
-//only registered users can login
+
+/*Task 7:     Complete the code for logging in as a registered user.	 */
 regd_users.post("/login", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
-
   if (!username || !password) {
       return res.status(404).json({message: "Error logging in"});
   }
@@ -53,31 +51,24 @@ regd_users.post("/login", (req,res) => {
   }
 });
 
-//  TASK 8 Complete the code for adding or modifying a book review.					 TASK 8
+/*  TASK 8 Complete the code for adding or modifying a book review.		*/			 
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Write your code here
     const username = req.body.username;
     const password = req.body.password;
-    const new_review = req.body.comment;
+    const put_review = req.body.review;
     if (!username || !password) {
         return res.status(404).json({message: "Error logging in"});
     }
      if (authenticatedUser(username,password)) {
        const isbn = req.params.isbn;
-       books[isbn]["reviews"][username]=new_review;
+       books[isbn]["reviews"][username]=put_review;
        return res.send(JSON.stringify(books[isbn],null,4)); 
        } else {   return res.status(208).json({message: "Invalid Login. Check username and password"});}
   });
-  
- /*    {"username":"user1","password":"pasw1"}
-     {"username":"user2","password":"pasw2"}
-     {"username":"user1","password":"pasw1","comment":"Very entertaining book"}
-     {"username":"user2","password":"pasw2","comment":"Excellent medieval work"}*/
-    
-//-----------------------------------------------------------
- //  TASK 9 Complete the code for deleting a book review.						TASK 9 
+ 
+/* TASK 9 Complete the code for deleting a book review. 	*/
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-    //Write your code here
+
     const username = req.body.username;
     const password = req.body.password;
     
@@ -87,19 +78,14 @@ regd_users.delete("/auth/review/:isbn", (req, res) => {
   
     if (authenticatedUser(username,password)) {
        const isbn = req.params.isbn;
-       let title=books[isbn]["title"];
-       let filtered_book=books[isbn]["isbn"];
-       let filtered_reviews= books[isbn]["reviews"];
        let delet_review = books[isbn]["reviews"][username];
        if (delet_review) {
            delete books[isbn]["reviews"][username];
        }
-      return res.send(JSON.stringify("The user: "+username+" has deleted the review of the book with title "+title,null,4)); 
+      return res.send(JSON.stringify("The user: "+username+" has deleted his review",null,4)); 
          } else {   return res.status(208).json({message: "Invalid Login. Check username and password"});}
   });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
-
-
